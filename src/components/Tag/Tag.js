ca6cd802d1ba1d5b125abb0d2112c33fb4c4ae5a
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-08-06 20:58:52 -0500

    chore: add Prop-Types to the project

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Tag.css';
+import PropTypes from 'prop-types';
 
 const tag = (props) => {
     return (
         <span className={`${props.color} tag`}>{props.text}</span>
     );
+};
+
+tag.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string.isRequired
 };
 
 export default tag;
